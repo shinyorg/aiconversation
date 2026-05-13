bad618c3051707e362fd20018cc873bfe39e1ee6
Allan Ritchie <allan.ritchie@gmail.com>
2026-05-13 17:47:49 -0400

    Update DocDbMessageStoreExtensions.cs

--- src/Shiny.AiConversation.MessageStores.SqliteDocDb/DocDbMessageStoreExtensions.cs
+++ src/Shiny.AiConversation.MessageStores.SqliteDocDb/DocDbMessageStoreExtensions.cs
@@ -12,12 +12,18 @@ public static class DocDbMessageStoreExtensions
     
     public static AiConversationOptions SetSqliteDocDbMessageStore(this AiConversationOptions options, string databasePath)
     {
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(databasePath);
+        
         options.SetMessageStore<DocumentDbMessageStore>();
+        var connString = databasePath;
+        if (!connString.StartsWith("Data Source="))
+            connString = "Data Source=" + connString;
+        
         options.Services.AddDocumentStore(SqliteDiRegKey, opts =>
         {
             opts.UseReflectionFallback = false;
             opts.JsonSerializerOptions = AppJsonContext.Default.Options;
-            opts.DatabaseProvider = new SqliteDatabaseProvider(databasePath);
+            opts.DatabaseProvider = new SqliteDatabaseProvider(connString);
         });
         return options;
     }
